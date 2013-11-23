0b8e8c67a6b0a685af3e5ed90572ebce4a724724
Louis LIN <louis.lin.87@gmail.com>
2013-11-23 22:35:18 +0100

    Initial import

---
+++ site-lutangular/webapp/WEB-INF/sql/plugins/directory/plugin/init_db_site-lutangular.sql
@@ -0,0 +1,5 @@
+INSERT INTO directory_directory VALUES (1,'Annuaire Foo','Annuaire Foo','<p>Annuaire indisponible.</p>','all',NULL,1,'2013-11-23 20:48:08',4,5,3,50,NULL,0,0,1,1,1,NULL,NULL,1,'',-1);
+INSERT INTO directory_entry VALUES (1,NULL,1,6,'Texte','','','',1,1,1,1,1,1,1,0,1,0,0,0,0,0,0,0,NULL,0,NULL,'',1,0,0,NULL),(2,NULL,1,5,'Liste','','','',1,1,0,0,0,1,1,0,2,0,0,0,0,0,1,0,NULL,0,NULL,'',0,1,0,NULL);
+INSERT INTO directory_record VALUES (1,'2013-11-23 20:48:22','2013-11-23 21:07:29',1,1,NULL,NULL),(2,'2013-11-23 21:07:37','2013-11-23 21:07:37',1,1,NULL,NULL);
+INSERT INTO directory_field VALUES (1,1,NULL,'',0,50,0,50,1,NULL,NULL,NULL,0,0),(2,2,'Elément 1','element_1',0,0,1,0,2,NULL,NULL,NULL,0,0),(3,2,'Elément 2','element_2',0,0,0,0,3,NULL,NULL,NULL,0,0);
+INSERT INTO directory_record_field VALUES (2,1,'element_1',2,2,NULL),(1,1,'foo',1,NULL,NULL),(3,2,'bar',1,NULL,NULL),(4,2,'element_2',2,3,NULL);
