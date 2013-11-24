4c2a45ec70d7ba8ac3e23374f870b6c40bf9cf0f
Louis LIN <louis.lin.87@gmail.com>
2013-11-24 18:24:07 +0100

    Init module-directory-angular

---
+++ module-directory-angular/src/java/fr/paris/lutece/plugins/directory/modules/angular/web/DirectoryAngularApp.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2002-2013, Mairie de Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
+package fr.paris.lutece.plugins.directory.modules.angular.web;
+
+import fr.paris.lutece.portal.web.xpages.XPageApplication;
+import fr.paris.lutece.portal.web.xpages.XPage;
+import fr.paris.lutece.portal.service.plugin.Plugin;
+import fr.paris.lutece.portal.service.message.SiteMessageException;
+import fr.paris.lutece.portal.service.security.UserNotSignedException;
+import fr.paris.lutece.util.html.HtmlTemplate;
+import fr.paris.lutece.portal.service.template.AppTemplateService;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * DirectoryAngularApp
+ */
+public class DirectoryAngularApp implements XPageApplication {
+    private static final String TEMPLATE_XPAGE_ANGULAR = "skin/plugins/directory/modules/angular/angular.html";
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public XPage getPage( HttpServletRequest request, int nMode, Plugin plugin ) throws SiteMessageException,
+            UserNotSignedException
+    {
+        XPage page = new XPage();
+
+        HtmlTemplate template = AppTemplateService.getTemplate( TEMPLATE_XPAGE_ANGULAR, request.getLocale( ) );
+        page.setContent( template.getHtml( ) );
+
+        return page;
+    }
+}
